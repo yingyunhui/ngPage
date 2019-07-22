b79a78189601063cb6a258cc9222755d140b4549
应云辉 <yingyunhui@YYh-MacBook-Pro.local>
2019-07-23 00:46:30 +0800

    add map

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { YYh } from '../providers/yyh';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule', canActivate: [YYh] },
+  //{ path: '', loadChildren: './tabs/tabs.module#TabsPageModule', canActivate: [YYh] },
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' }
 ];
 @NgModule({
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+declare const BMap: any;
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
@@ -8,5 +9,16 @@ import { Component } from '@angular/core';
 export class Tab3Page {
 
   constructor() {}
+  
+  ngOnInit() {
+    let map = new BMap.Map("cjmap");
+    map.centerAndZoom(new BMap.Point(121.62426,29.866786), 13);  // 初始化地图,设置中心点坐标和地图级别
+	  map.setCurrentCity("宁波");          // 设置地图显示的城市 此项是必须设置的
+	  map.enableScrollWheelZoom(true);     //开启鼠标滚轮缩放
+  }
+
+  segmentChanged(event){
+    console.log(event.target.value)
+  }
 
 }
